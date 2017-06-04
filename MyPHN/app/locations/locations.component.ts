d0755af1d2475da8fa0ae4f81dd7f37b29fce60e
Joe Pleso <joseph.pleso@philips.com>
2017-06-04 17:12:14 -0400

    feat(*): add county cards

--- MyPHN/app/locations/locations.component.ts
+++ MyPHN/app/locations/locations.component.ts
@@ -1,47 +1,28 @@
 import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 import { View } from "ui/core/view";
 import { LocationsService } from "../shared/location/locations.service";
-
-
-import { Color } from "color";
-
-
-declare var CGSizeMake: any;
+import { County } from "../shared/location/county";
 
 @Component({
     selector: "ns-items",
     moduleId: module.id,
     templateUrl: "./locations.component.html",
+    styleUrls: ["./locations.component.css"]
 })
 
-
 export class LocationsComponent implements OnInit {
-    public tabSelectedIndex: number;
-    thing: number
+
+    public counties: Array<County>;
+
     constructor(private locationsService: LocationsService) {
-        this.tabSelectedIndex = 3;
+
     }
 
     ngOnInit(): void {
-        //this.items = this.itemService.getItems();
-        this.thing = 0;
-        /*this.locationsService.getCountyLocations().then((x) => {
-            let str = JSON.stringify(x, null, 4)
-            console.log(str);
-        },
-            (error) => alert("Unfortunately we could not find your account.")
-        );*/
         this.locationsService.getCounties().then((x) => {
-            console.log(x[0].getName(), x[0].getHref());
+            this.counties = x;
         },
-            (error) => alert("Unfortunately we could not find your account." + error)
+            (error) => alert("Could not load location info." + error)
         );
-
-
-    }
-
-    change(): void {
-        this.tabSelectedIndex = 3;
-        console.log(this.tabSelectedIndex);
     }
 }
