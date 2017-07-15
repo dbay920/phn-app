793d91dcb52d4c7e6400b05b3808b99b31baed80
Joe Pleso <joseph.pleso@philips.com>
2017-07-15 16:46:19 -0400

    feat(*): link with native maps for directions

--- MyPHN/app/locations/detail.component.ts
+++ MyPHN/app/locations/detail.component.ts
@@ -5,6 +5,10 @@ import { LocationsService } from "../shared/location/locations.service";
 import { LocationDetail } from "../shared/location/detail";
 import { ActivatedRoute, Router } from '@angular/router';
 import { PageRoute } from "nativescript-angular/router";
+import { isEnabled, enableLocationRequest, getCurrentLocation, watchLocation, distance, clearWatch } from "nativescript-geolocation";
+import { LocateAddress } from "nativescript-locate-address";
+
+
 
 @Component({
     selector: "ns-items",
@@ -33,6 +37,19 @@ export class LocationDetailComponent implements OnInit {
 
 
     ngOnInit(): void {
+        if (!isEnabled()) {
+            enableLocationRequest();
+            console.log('nonblocking');
+        }
+
+        let x = getCurrentLocation({});
+        x.then((loc) => {
+            if (loc) {
+                console.log("Current location is: " + loc.latitude + loc.longitude);
+
+            }
+        });
+
         this.locationsService.getLocationDetails(this.locationId).then((x) => {
             //this.locations = x;
             let str = JSON.stringify(x, null, 4)
@@ -49,10 +66,21 @@ export class LocationDetailComponent implements OnInit {
             console.log(this.image);
             this.address = this.details.getAddress().replace(',', '\n');
             this.name = this.details.getName();
-
             //console.log(this.details.getProviders());
         },
             (error) => alert("Could not load location details.")
         );
+    }
+
+    navigate() {
+        let locator = new LocateAddress();
+        locator.locate({
+            address: this.details.getAddress(),
+        }).then(() => {
+            console.log("Maps app launched.");
+        }, (error) => {
+            console.log(error);
+        });
+
     }
 }
