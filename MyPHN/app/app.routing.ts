e2f263265d0955c77536d78229421fec946ecc7c
Joe Pleso <joseph.pleso@philips.com>
2017-07-31 20:54:09 -0400

    whoops. thanks GitHub

--- MyPHN/app/app.routing.ts
+++ MyPHN/app/app.routing.ts
@@ -6,6 +6,7 @@ import { LocationDetailComponent } from "./locations/detail.component";
 import { HomeComponent } from "./home/home.component";
 import { CountyComponent } from "./locations/county.component";
 import { ServicesComponent } from "./services/services.component"
+import { ServiceDetailComponent } from "./services/detail.component"
 import { SearchComponent } from "./search/search.component"
 import { SearchTermComponent } from "./search/term.component"
 import { PortalComponent } from "./portal/portal.component"
@@ -43,6 +44,7 @@ const routes: Routes = [
 
             // services
             { path: 'services', component: ServicesComponent },
+            { path: 'services/:id', component: ServiceDetailComponent },
 
             // providers
             { path: 'providers', component: ServicesComponent },
