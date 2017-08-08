8e9f3c08f8b37f354794c4c2db037812c8b08523
Joe Pleso <joseph.pleso@philips.com>
2017-08-08 16:02:29 -0400

    feat: stub search term component

--- MyPHN/app/search/term.component.ts
+++ MyPHN/app/search/term.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { QueryList, Component, ViewChildren, AfterViewInit } from "@angular/core";
 import { ActivatedRoute, Router } from '@angular/router';
+import { WebView } from "ui/web-view"
+
 
 @Component({
     selector: "ns-items",
@@ -7,9 +9,46 @@ import { ActivatedRoute, Router } from '@angular/router';
     templateUrl: "./term.component.html",
 })
 
-export class SearchTermComponent {
+export class SearchTermComponent implements AfterViewInit {
     webViewSrc;
     searchTerm;
+
+    //@ViewChild(WebView) public wv: WebView;
+    public static tabView;
+    @ViewChildren('ref') ref: QueryList<any>;
+
+    isLocation(href) {
+        console.log(href);
+
+        return href.indexOf('LocationDetail.aspx?id=') >= 0;
+    }
+
+    ngAfterViewInit() {
+        /*this.drawer = this.drawerComponent.sideDrawer;
+        ItemsComponent.tabView = this.ref.first.nativeElement;
+        //ItemsComponent.tabView.items[3].addEventListener('tap', this.hi);
+        //        ItemsComponent.tabView.items.push(new TabViewItem());
+
+        let uiTabBarCtrl = ItemsComponent.tabView.ios;
+        let view = ItemsComponent.tabView.nativeView;*/
+        this.ref.first.nativeElement.on(WebView.loadStartedEvent, (event) => {
+            if (this.isLocation(event.url)) {
+
+                // TODO: android logic
+                // Stop the loading event
+                //if (ios) {
+                event.object.ios.stopLoading();
+                //} else if (android) {
+                //   event.object.android.stopLoading();
+                // }
+
+                // Do something depending on the coordinates in the URL
+                this._router.navigateByUrl("items/locations/detail/" +
+                    event.url.split('=')[1]);
+            }
+        });
+    }
+
 
     constructor(
         private _router: Router,
