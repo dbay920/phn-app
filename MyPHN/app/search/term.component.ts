35b3164842960ecba2c5e7a6d4e14e4c94c4c0ac
Joe Pleso <joseph.pleso@philips.com>
2017-08-16 21:43:29 -0400

    refactor(term): clean up dead code

--- MyPHN/app/search/term.component.ts
+++ MyPHN/app/search/term.component.ts
@@ -1,7 +1,6 @@
-import { QueryList, Component, ViewChildren, AfterViewInit } from "@angular/core";
+import { QueryList, Component, ViewChildren, AfterViewInit, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from '@angular/router';
 import { WebView } from "ui/web-view"
-
 
 @Component({
     selector: "ns-items",
@@ -9,38 +8,35 @@ import { WebView } from "ui/web-view"
     templateUrl: "./term.component.html",
 })
 
-export class SearchTermComponent implements AfterViewInit {
-    webViewSrc;
-    searchTerm;
-
-    //@ViewChild(WebView) public wv: WebView;
+export class SearchTermComponent implements AfterViewInit, OnInit {
+    public webViewSrc;
+    public searchTerm;
     public static tabView;
     @ViewChildren('ref') ref: QueryList<any>;
 
     isLocation(href) {
-        console.log(href);
-
         return href.indexOf('LocationDetail.aspx?id=') >= 0;
     }
 
-    ngAfterViewInit() {
-        /*this.drawer = this.drawerComponent.sideDrawer;
-        ItemsComponent.tabView = this.ref.first.nativeElement;
-        //ItemsComponent.tabView.items[3].addEventListener('tap', this.hi);
-        //        ItemsComponent.tabView.items.push(new TabViewItem());
+    ngOnInit() {
+        this.route.params.forEach((params) => {
+            this.searchTerm = params["term"];
+        });
 
-        let uiTabBarCtrl = ItemsComponent.tabView.ios;
-        let view = ItemsComponent.tabView.nativeView;*/
+        this.webViewSrc = 'https://primary-health.net/Search.aspx?q='
+            + this.searchTerm;
+    }
+
+    ngAfterViewInit() {
         this.ref.first.nativeElement.on(WebView.loadStartedEvent, (event) => {
             if (this.isLocation(event.url)) {
 
-                // TODO: android logic
                 // Stop the loading event
-                //if (ios) {
-                event.object.ios.stopLoading();
-                //} else if (android) {
-                //   event.object.android.stopLoading();
-                // }
+                if (!android) {
+                    event.object.ios.stopLoading();
+                } else {
+                    event.object.android.stopLoading();
+                }
 
                 // Do something depending on the coordinates in the URL
                 this._router.navigateByUrl("items/locations/detail/" +
@@ -49,17 +45,8 @@ export class SearchTermComponent implements AfterViewInit {
         });
     }
 
-
     constructor(
         private _router: Router,
         private route: ActivatedRoute
-    ) {
-        this.route.params.forEach((params) => {
-            this.searchTerm = params["term"];
-        });
-
-        this.webViewSrc = 'https://primary-health.net/Search.aspx?q='
-            + this.searchTerm + '#resInfo-2';
-        console.log(this.webViewSrc)
-    }
+    ) { }
 }
