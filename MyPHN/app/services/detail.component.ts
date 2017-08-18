2400b15b458f488d5a89b73a25cdd4db2ae23d0e
Joe Pleso <joseph.pleso@philips.com>
2017-08-18 01:55:51 -0400

    feat: make service detail component use web view

--- MyPHN/app/services/detail.component.ts
+++ MyPHN/app/services/detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
-import { ServicesService } from "../shared/services/services.service";
+import { QueryList, Component, OnInit, ElementRef, ViewChildren, AfterViewInit } from "@angular/core";
+//import { ServicesService } from "../shared/services/services.service";
 import { ActivatedRoute, Router } from '@angular/router';
-import { ServiceDetail } from '../shared/services/detail'
+//import { ServiceDetail } from '../shared/services/detail'
+import { WebView } from "ui/web-view"
 
 @Component({
     selector: "ns-items",
@@ -10,22 +11,48 @@ import { ServiceDetail } from '../shared/services/detail'
     styleUrls: ["./detail.component.css"]
 })
 
-export class ServiceDetailComponent implements OnInit {
-    id: string;
-    detail: ServiceDetail
-    webViewSrc;
+export class ServiceDetailComponent implements OnInit, AfterViewInit {
+    public id: string;
+    //detail: ServiceDetail
+    public webViewSrc: string;
+    @ViewChildren('ref') ref: QueryList<any>;
 
     constructor(
         private route: ActivatedRoute,
         private _router: Router,
         //private servicesService: ServicesService
-    ) {
-        this.route.params
-            .forEach((params) => {
-                this.id = params["id"];
-            });
-        console.log(typeof this.id);
-        this.webViewSrc = 'https://primary-health.net/ServiceDetail.aspx?id=' + this.id;
+    ) { }
+
+    isLocation(href) {
+        return href.indexOf('LocationDetail.aspx?id=') >= 0;
+    }
+
+    isProvider(href) {
+        return href.indexOf('DoctorBio.aspx?id=') >= 0;
+    }
+
+    ngAfterViewInit() {
+        this.ref.first.nativeElement.on(WebView.loadStartedEvent, (event) => {
+            if (this.isLocation(event.url) || this.isProvider(event.url)) {
+
+                // Stop the loading event
+                if (!android) {
+                    event.object.ios.stopLoading();
+                } else {
+                    event.object.android.stopLoading();
+                }
+
+                // Do something depending on the coordinates in the URL
+                if (this.isLocation(event.url)) {
+                    this._router.navigateByUrl("items/locations/detail/" +
+                        event.url.split('=')[1]);
+                } else {
+                    // is provider
+                    this._router.navigateByUrl("items/providers/" +
+                        event.url.split('=')[1]);
+                }
+            }
+        });
     }
 
     /*listViewItemTap(i): void {
@@ -37,6 +64,12 @@ export class ServiceDetailComponent implements OnInit {
     }*/
 
     ngOnInit(): void {
+        this.route.params.forEach((params) => {
+            this.id = params["id"];
+        });
+
+        this.webViewSrc = 'https://primary-health.net/ServiceDetail.aspx?id=' + this.id;
+
         /*        this.servicesService.getDetails(this.id).then((x) => {
                     this.detail = x;
                 },
