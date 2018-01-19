1d202e2394aa9580db04f8d3e6c56c9fdeb0e37a
Joe Pleso <elimin8r@users.noreply.github.com>
2018-01-19 18:50:16 -0500

    feat(*): remove healthCenter.js

--- MyPHN/app/shared/config.ts
+++ MyPHN/app/shared/config.ts
@@ -1,31 +1,9 @@
-var fetchModule = require("fetch");
-
 export class Config {
 
-    static healthCenters;
     static messageSenderId = '453784066967'; // used for push notifications
 
     static initialize() {
-        let href = 'https://primary-health.net/PHN_SiteLocator/HealthCenters.js'
-        fetchModule.fetch(href,
-            {
-                method: "GET"
-            })
-            .then(this.handleErrors)
-            .then((x) => {
-                return x.text();
-            })
-            .then((x) => {
-                x = x.slice(19).split(';')[0];
-                Config.healthCenters = JSON.parse(x);
-            });
-    }
 
-    private static handleErrors(response) {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        return response;
     }
 }
 Config.initialize();
